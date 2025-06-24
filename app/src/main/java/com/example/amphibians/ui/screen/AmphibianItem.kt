590440088c5709a63b739b4a20ca48e5f9512095
rintarotajima <rintarotajima@gmail.com>
2025-06-24 17:36:28 +0900

    UI要素の作成

--- app/src/main/java/com/example/amphibians/ui/screen/AmphibianItem.kt
+++ app/src/main/java/com/example/amphibians/ui/screen/AmphibianItem.kt
@@ -1,10 +1,12 @@
 package com.example.amphibians.ui.screen
 
-import android.R
-import androidx.compose.foundation.Image
+import com.example.amphibians.R
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -14,16 +16,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import coil3.compose.rememberAsyncImagePainter
 import coil3.request.ImageRequest
 import coil3.request.crossfade
 import com.example.amphibians.model.Amphibian
+import com.example.amphibians.ui.theme.AmphibiansTheme
 
 @Composable
 fun AmphibianItem(amphibian: Amphibian, modifier: Modifier = Modifier) {
@@ -35,7 +37,10 @@ fun AmphibianItem(amphibian: Amphibian, modifier: Modifier = Modifier) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = amphibian.name,
+                text = stringResource(R.string.amphibian_title, amphibian.name,  amphibian.type),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_medium)),
                 style = MaterialTheme.typography.titleLarge,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Start
@@ -50,9 +55,25 @@ fun AmphibianItem(amphibian: Amphibian, modifier: Modifier = Modifier) {
             )
             Text(
                 text = amphibian.description,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
                 style = MaterialTheme.typography.titleMedium,
                 textAlign = TextAlign.Justify,
             )
+        }
+    }
+}
+
+@Composable
+private fun AmphibianList(
+    amphibians: List<Amphibian>,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn {
+        items(
+            items = amphibians,
+            key = { amphibian -> amphibian.name }
+        ) { amphibian ->
+            AmphibianItem(amphibian = amphibian, modifier = modifier.fillMaxSize())
         }
     }
 }
@@ -65,7 +86,23 @@ fun AmphibianItemPreview() {
             name = "Great Basin Spadefoot",
             type = "Toad",
             description = "This toad spends most of its life underground due to the arid desert conditions in which it lives. Spadefoot toads earn the name because of their hind legs which are wedged to aid in digging. They are typically grey, green, or brown with dark spots.",
-            imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/great-basin-spadefoot.png"
+            imgSrc = ""
         )
     )
+}
+
+@Preview
+@Composable
+fun AmphibianListPreview() {
+    AmphibiansTheme {
+        val mockData = List(10) {
+            Amphibian(
+                "Lorem Ipsum - $it",
+                "$it",
+                "Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+                imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/great-basin-spadefoot.png"
+            )
+        }
+        AmphibianList(amphibians = mockData, modifier = Modifier.fillMaxSize())
+    }
 }
