d0f5094fc423ca61ab362143036616fc1fcae473
rintarotajima <rintarotajima@gmail.com>
2025-06-26 13:37:23 +0900

    UIレイヤーの完成

---
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
@@ -0,0 +1,47 @@
+package com.example.amphibians.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.amphibians.R
+import com.example.amphibians.ui.screen.AmphibiansViewModel
+import com.example.amphibians.ui.screen.HomeScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibiansApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.amphibian_title),
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                }
+            )
+        }
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            val amphibiansViewModel: AmphibiansViewModel = viewModel()
+            HomeScreen(
+                amphibiansUiState = amphibiansViewModel.amphibiansUiState,
+                retryAction = {},
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = it
+            )
+        }
+    }
+}
